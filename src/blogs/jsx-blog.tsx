b91317c40e8686c7f943eea2263901275540b2b9
MichaelDuo <michaeldong21@gmail.com>
2021-02-16 22:00:54 -0800

    update jsx blog

--- src/blogs/jsx-blog.tsx
+++ src/blogs/jsx-blog.tsx
@@ -22,6 +22,17 @@ const Test = (): JSX.Element => {
 		<Layout>
 			<Header>This is a jsx Blog</Header>
 			<p>Counter: {count}</p>
+			<div
+				dangerouslySetInnerHTML={{
+					__html: `
+					<iframe height="500" style="width: 100%;" scrolling="no" title="Lantern" src="https://codepen.io/michaelduo/embed/Pozpzay?height=265&theme-id=light&default-tab=css,result" frameborder="no" loading="lazy" allowtransparency="true" allowfullscreen="true">
+					See the Pen <a href='https://codepen.io/michaelduo/pen/Pozpzay'>Lantern</a> by Michael Dong
+					(<a href='https://codepen.io/michaelduo'>@michaelduo</a>) on <a href='https://codepen.io'>CodePen</a>.
+				  </iframe>
+			  
+			`,
+				}}
+			></div>
 		</Layout>
 	);
 };
